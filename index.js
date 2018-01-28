d58034dd743d7d086d92217ade752908e6643f7b
LaneMiller <chris.lane.miller@gmail.com>
2018-01-28 21:25:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma (string) {
-  if lowercase return "I can't hear you!"
-  if uppercade return "YES INDEED!"
-  if string = "I love you, Grandma." return "I love you, too."
+  if (lowercase) return "I can't hear you!"
+  if (uppercase) return "YES INDEED!"
+  if ("I love you, Grandma.") return "I love you, too."
 }
