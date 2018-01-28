0e2fe17a4014d6ccda785a9675a9a399d350c6ff
LaneMiller <chris.lane.miller@gmail.com>
2018-01-28 21:27:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma (string) {
+  var uppercase = "HELLO!"
+  uppercase.toUpperCase() === uppercase
+  
+  var lowercase = "hello!"
+  lowercase.toLowerCase() === lowercase
+  
+  var mixedCase = "Hi There!"
+  mixedCase.toLowerCase() === lowercase
+  mixedCase.toUpperCase() === uppercase
+  
   if (lowercase) return "I can't hear you!"
   if (uppercase) return "YES INDEED!"
   if ("I love you, Grandma.") return "I love you, too."
